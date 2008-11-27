ddfd72824eac6a5fe8be261bfe6ab2557406ce43
Benjamin Clauss <email@benjaminclauss.de>
2008-11-27 21:36:22 +0000

    jetzt sortierts richtig rum!
    git-svn-id: file:///Users/sst/studienarbeit_svn/trunk/StockCutter@60 852b387f-191d-405f-8874-2b06c2047695

--- src/scp/common/ShapeSortByIdComparator.java
+++ src/scp/common/ShapeSortByIdComparator.java
@@ -6,9 +6,9 @@ public class ShapeSortByIdComparator implements Comparator<Shape> {
 
 	public int compare(Shape s1, Shape s2) {
 		if (s1.getId() > s2.getId()) {
-			return -1;
-		} else if (s1.getId() < s2.getId()) {
 			return 1;
+		} else if (s1.getId() < s2.getId()) {
+			return -1;
 		}
 		
 		return 0;
